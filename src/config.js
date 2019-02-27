effc1f5e82182b8a183ae8ae699b2620aede5577
SCWR <dl_run@qq.com>
2019-02-27 16:11:34 +0800

    change scripts fix bug

--- src/config.js
+++ src/config.js
@@ -4,17 +4,17 @@ import {quickQuoteConfig} from '../quick-quote.config.js'
 const ROOT_PATH = process.cwd()
 
 export function loadConfig (key) {
-  let config
+  let config = quickQuoteConfig
   const configPath = path.resolve(ROOT_PATH, 'quick-quote.config.js')
   if (fs.existsSync(configPath)) {
     try {
       // eslint-disable-next-line global-require
-      config = require(configPath)
-      if (!config || !config.quickQuoteConfig) {
+      let userConfig = require(configPath)
+      if (!userConfig || !userConfig.quickQuoteConfig) {
         console.log(`It is recommended to use the babel-plugin-quick-quote command,
         generate the corresponding configuration file(quick-quote.config.js) for the echarts version currently used by the project.
         https://github.com/SCWR/babel-plugin-quick-quote#readme`)
-        config = quickQuoteConfig
+        config = userConfig.quickQuoteConfig
       }
     } catch (e) {
       console.log(e)
